430d1aaee9ad6665b63ed4e5e0abc5536a8cd401
rolandiartmeladze <rartmeladze@gmail.com>
2024-03-24 21:07:25 +0400

    commit finde

--- src/component/Aside.tsx
+++ src/component/Aside.tsx
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import '../style/Aside.css';
 import  FindIcon from '../icon/find.png';
-
-
 import debounce from 'lodash.debounce'; 
+import styled from 'styled-components';
+
+
+const LoadConteiner = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(5.5px);
+  bottom: 0;
+  font-size: 200%;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 5;
+`;
 
 
 
@@ -29,7 +43,7 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFindInput(newValue);
 
             if(!loading){
-                           findRequest();
+                findRequest();
             }
 
 };
@@ -65,6 +79,7 @@ const findRequest = async () => {
 
         return (
 <>
+
 <div className='Aside'>
     <h1>User info </h1>
     <div className='Finde'>
@@ -96,6 +111,10 @@ const findRequest = async () => {
 
 
 </div>
+
+{loading ? 
+                <LoadConteiner> {"Loaging..."} </LoadConteiner>:null
+}
 </>
         );
 }
