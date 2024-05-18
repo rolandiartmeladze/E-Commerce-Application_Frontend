65df51133c183610dbac22356348a666fef88ed7
rolandiartmeladze <rartmeladze@gmail.com>
2024-05-18 06:04:07 +0400

    optmise deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom';
 
 
 import './App.css';
